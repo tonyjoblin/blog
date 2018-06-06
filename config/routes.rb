345e7483fd88d3c2dbb318d5a0e07276e8c450ee
Tony Joblin <tonyjoblin@yahoo.com.au>
2018-06-07 05:17:10 +1000

    Added route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@
 #
 #                    Prefix Verb   URI Pattern                                                                              Controller#Action
 #                      root GET    /                                                                                        articles#index
+#          article_comments GET    /articles/:article_id/comments(.:format)                                                 comments#index
+#                           POST   /articles/:article_id/comments(.:format)                                                 comments#create
+#       new_article_comment GET    /articles/:article_id/comments/new(.:format)                                             comments#new
+#      edit_article_comment GET    /articles/:article_id/comments/:id/edit(.:format)                                        comments#edit
+#           article_comment GET    /articles/:article_id/comments/:id(.:format)                                             comments#show
+#                           PATCH  /articles/:article_id/comments/:id(.:format)                                             comments#update
+#                           PUT    /articles/:article_id/comments/:id(.:format)                                             comments#update
+#                           DELETE /articles/:article_id/comments/:id(.:format)                                             comments#destroy
 #                  articles GET    /articles(.:format)                                                                      articles#index
 #                           POST   /articles(.:format)                                                                      articles#create
 #               new_article GET    /articles/new(.:format)                                                                  articles#new
@@ -19,5 +27,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
