041216bfd42d776cb5ac588a22572c5f0bba540c
Tony Joblin <tonyjoblin@yahoo.com.au>
2018-06-06 21:08:21 +1000

    Added validation on article title
    Check presence and minimum length of title.
    Added check on save in controller action
    Updated new article view to display errors.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
   # Creates a new article and redirects to index
   def create
     @article = Article.new new_article_params
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def new_article_params
